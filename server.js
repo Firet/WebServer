ba6b7be09e62cbcec68c12cd5c2c7a85c2c59ede
Firet <damianfcardozo@gmail.com>
2018-08-10 11:32:59 -0300

    agrego MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
-app.get('/home', (req, res) => res.send('Home'))
+app.use(cors())
+const usuarios = require("./data/usuarios.json")
 
-app.listen(3030, () => console.log('Example app listening on port 3030!'))
+app.get('/usuarios', (req, res) => res.send(usuarios))
+
+
+
+// Connection URL
+const url = 'mongodb://ws:53yer64trw23542s@ds117422.mlab.com:17422/clasesprog';
+console.log("Conectando a mlab...")
+MongoClient.connect(url, function(err, client) {
+    if(!err){
+        console.log("Conectado!")
+        app.listen(3000, () => console.log('Servidor iniciado en el puerto 3000!'))
+    }else{
+        console.log("Imposible conectar con mlab")
+    }
+});
